fdfa5897cb7ff50bda90d0c36dfaec51abbb014b
Arya Enrico <aryaenrico789@gmail.com>
2023-05-18 23:23:51 +0700

    smell code tf-idf

--- app/controller/api/v1/clasification.js
+++ app/controller/api/v1/clasification.js
@@ -8,6 +8,14 @@ module.exports = {
     let dataset = [];
     let feature;
     let number_feature = [];
+    let datasetNegatif=[];
+    let datasetPositif=[];
+    let tfHs=[];
+    let dfNHs=[];
+    let idfHs=[];
+    let wHs=[];
+    let wNhs=[];
+    
 
     const data = await service.getData();
     for (i = 0; i < data.length; i++) {
@@ -23,36 +31,84 @@ module.exports = {
       }
     }
     feature = [...bag_of_word];
+
+    for (i=0;i<dataset.length;i++){
+      dataset[i].klasifikasi == 'hs' ? datasetNegatif.push(dataset[i]):  datasetPositif.push(dataset[i]);
+    }
+
+    // perhitungan tf kelas hate speech
+    for (i=0;i<datasetPositif.length;i++){
+      let doc = datasetPositif[i].tweet.split(" ");
+      let num=[];
+      for (j=0;j<feature.length;j++){
+          let score =0;
+          for (k=0;k<doc.length;k++){
+            if (doc[k] == feature[j]){
+               score++;
+            }
+          }
+          num.push((score/doc.length).toFixed(3));
+      }
+      tfHs.push(num);
+    }
+
+    // perhitungan df 
+    for (i=0;i<datasetPositif.length;i++){
+      let doc = datasetPositif[i].tweet.split(" ");
+      let num=[];
+      for (j=0;j<feature.length;j++){
+          let score =0;
+          for (k=0;k<doc.length;k++){
+            if (doc[k] == feature[j]){
+               score++;
+               break;
+            }
+          }
+          num.push(score);
+      }
+      dfNHs.push(num);
+    }
+
+     // perhitungan idf 
+     for (i=0;i<feature.length;i++){
+      let documentCount =0;
+      for (j=0;j<dfNHs.length;j++){
+        if (dfNHs[j][i] > 0){
+          documentCount++;
+         }
+      }
+      documentCount == 0 ?idfHs.push(0) : idfHs.push(Math.log(dfNHs.length/documentCount));
+      
+    }
+
   
 
-    for (i = 0; i < 2; i++) {
+    // for (i = 0; i < 2; i++) {
                  
-      let temp = dataset[i].tweet.split(" ");
-      let num = [];
-      for (k = 0; k < feature.length; k++) {
-        let score = 0;
-        for (j = 0; j < temp.length; j++) {
-             if (temp[j] == feature[k]){
-                    score ++;
-             }
-        }
-        num.push((score/temp.length).toFixed(2));
-      }
-      number_feature.push(num);
-        
-
-     
-    }
+    //   let temp = dataset[i].tweet.split(" ");
+    //   let num = [];
+    //   for (k = 0; k < feature.length; k++) {
+    //     let score = 0;
+    //     for (j = 0; j < temp.length; j++) {
+    //          if (temp[j] == feature[k]){
+    //                 score ++;
+    //          }
+    //     }
+    //     num.push((score/temp.length).toFixed(2));
+    //   }
+    //   number_feature.push(num);
+    // }
       
     
 
     res.status(200).json({
       status: "ok",
       message: "berhasil",
-      bag_of_word: number_feature,
-      data:feature.length,
-      kata:dataset[1].tweet,
-      word:feature
+      bag_of_word: feature,
+      tf:tfHs,
+      df:dfNHs,
+      idfHs:idfHs,
+      kata:datasetPositif[0].tweet
     });
   },
 };
---
+++ utils/featureExtraction.js
@@ -0,0 +1,4 @@
+async function createArraytf(dataset,feature,operation){
+
+
+}
