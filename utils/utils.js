64efc25dd5355833796e362fa91ec1253b3055f0
Arya Enrico <aryaenrico789@gmail.com>
2023-07-15 22:56:17 +0700

    feature 2angka menjadi huruf

--- utils/utils.js
+++ utils/utils.js
@@ -19,23 +19,35 @@ function removeExcesSpace(text) {
 function removeMention(text) {
   return text.replace(/[@]{1}(.*?) /g, "");
 }
-function removeNumber(text){
+function removeNumber(text) {
   return text.replace(/[4]/g, "A");
 }
-function replaceNumberToCharacter(text){
-  let resultWord ="";
+function replaceNumberToCharacter(text) {
+  let resultWord = "";
   let word = removeExcesSpace(text).split(" ");
-  for (let i=0;i<word.length;i++){
-      if (/[0-9]/g.test(word[i])){
-            for (let k=0;k<word[i].length;k++){
-              let temp = word[i];
-              if(/[0-9]/g.test(temp.charAt(k))){
-                //word[i].charAt(k) = mappingNumberToCharacter(word[i].charAt(k));
-                word[i] = word[i].substring(0, k) + mappingNumberToCharacter(word[i].charAt(k)) + word[i].substring(k + 1);
-
-              }
-            }
+  for (let i = 0; i < word.length; i++) {
+    if (/[0-9]/g.test(word[i])) {
+      for (let k = 0; k < word[i].length; k++) {
+        let temp = word[i];
+        if (/[0-9]/g.test(temp.charAt(k))) {
+          if (
+            temp.charAt(k) == "1" &&
+            (temp.charAt(k + 1) == "2" || temp.charAt(k + 1) == "3")
+          ) {
+            let param = `${temp.charAt(k)}${temp.charAt(k + 1)}`;
+            word[i] =
+              word[i].substring(0, k) +
+              mappingNumberToCharacter(param) +
+              word[i].substring(k + 2);
+          } else {
+            word[i] =
+              word[i].substring(0, k) +
+              mappingNumberToCharacter(word[i].charAt(k)) +
+              word[i].substring(k + 1);
+          }
+        }
       }
+    }
   }
   for (kata of word) {
     resultWord = `${resultWord} ${kata}`;
@@ -50,11 +62,11 @@ function removeLink(text) {
   return text.replace(/(https:\/\/){1}(.*?) /g, "");
 }
 
-function removeWordNya(text){
+function removeWordNya(text) {
   let word = text.split(" ");
-  let result="";
-  for (i=0;i<word.length;i++){
-    result =`${result} ${word[i].replace('nya','')}`;
+  let result = "";
+  for (i = 0; i < word.length; i++) {
+    result = `${result} ${word[i].replace("nya", "")}`;
   }
   return result;
 }
@@ -63,9 +75,8 @@ function removeLineBreak(text) {
   return text.replace(/(\r\n|\n|\r)/gm, " ");
 }
 
-function removeOnlyOneCharacter(text){
-  return text.replace(/ [a-z]{1,3} /g," ");
- 
+function removeOnlyOneCharacter(text) {
+  return text.replace(/ [a-z]{1,3} /gm, " ");
 }
 
 function removePunctuation(text) {
@@ -90,48 +101,64 @@ function operationLower({ tanggal, tweet, klasifikasi }) {
   return new Dataset(tanggal, caseFolding(tempTweet), klasifikasi);
 }
 
-function mappingNumberToCharacter(text){
-  let result ="";
-  switch(text){
-    case "1": 
-    result ="i";
-    break;
-    case "5":
-      result ="s"
+function mappingNumberToCharacter(text) {
+  let result = "";
+  switch (text) {
+    case "1":
+      result = "i";
       break;
-      case "4":
-        result ="a"
-        break;
-        case "3":
-          result ="e"
-          break;
+    case "2":
+      result = "z";
+      break;
+    case "5":
+      result = "s";
+      break;
+    case "4":
+      result = "a";
+      break;
+    case "3":
+      result = "e";
+      break;
+    case "6":
+      result = "g";
+      break;
+    case "7":
+      result = "j";
+      break;
+    case "8":
+      result = "b";
+      break;
+    case "9":
+      result = "g";
+      break;
+    case "12":
+      result = "r";
+      break;
+    case "13":
+      result = "b";
+      break;
   }
   return result;
 }
 
-// function onlyText(text){
-//   return text.replace(/[^a-z]/g, " ");
-// }
+function onlyText(text){
+  return text.replace(/[^a-z]/g, " ");
+}
 
-function operationMention({ tanggal, tweet, klasifikasi },code=1) {
+function operationMention({ tanggal, tweet, klasifikasi }, code = 1) {
   let tempTweet = `${tweet} `;
   let resultMention = removeMention(tempTweet);
   let resultLink = removeLink(resultMention);
   let resultHastag = removeHastag(resultLink);
-  let removeTandaBaca =removePunctuation(resultHastag);
+  let removeTandaBaca = removePunctuation(resultHastag);
   let replaceToCharacter = replaceNumberToCharacter(removeTandaBaca);
-  //let resutltNumber = removeNumber(resultHastag);
- // let replaceToCharacter =replaceNumberToCharacter(resutltNumber)
-  //let resultRemoveEmote =onlyText(resutltNumber);
- // let removeTandaBaca =removePunctuation(resutltNumber);
-  let tempTweet2 = ` ${replaceToCharacter} `;
-  if (code == 1){
-    return new Dataset(tanggal,removeExcesSpace(tempTweet2), klasifikasi);
-  }else{
-    return removeExcesSpace(tempTweet2)
+  let resultRemoveEmote =onlyText(replaceToCharacter);
+  let tempTweet2 = ` ${resultRemoveEmote} `;
+  if (code == 1) {
+    return new Dataset(tanggal, removeExcesSpace(tempTweet2), klasifikasi);
+  } else {
+    return removeExcesSpace(tempTweet2);
   }
-
-  
 }
 
 function operationSlangAndStopWord(data, code, map) {
@@ -142,70 +169,72 @@ function operationSlangAndStopWord(data, code, map) {
     for (j = 0; j < tweetSplit.length; j++) {
       let word = map.get(tweetSplit[j]);
       if (word != undefined) {
-        code == 1 ? (tweetSplit[j] = word) : tweetSplit[j]="";
+        code == 1 ? (tweetSplit[j] = word) : (tweetSplit[j] = "");
       }
     }
     for (kata of tweetSplit) {
       resultWord = `${resultWord} ${kata}`;
     }
-   if (code == 1){
-    arrayWord.push(
-      new Dataset(data[i].tanggal, removeExcesSpace(resultWord.trim()), data[i].klasifikasi)
-    );
-   }else{
-    arrayWord.push(
-      new Dataset(data[i].tanggal, removeExcesSpace(removeOnlyOneCharacter(resultWord.trim())), data[i].klasifikasi)
-    );
-   }
-    
+    if (code == 1) {
+      arrayWord.push(
+        new Dataset(
+          data[i].tanggal,
+          removeExcesSpace(resultWord.trim()),
+          data[i].klasifikasi
+        )
+      );
+    } else {
+      arrayWord.push(
+        new Dataset(
+          data[i].tanggal,
+          removeExcesSpace(removeOnlyOneCharacter(resultWord.trim())),
+          data[i].klasifikasi
+        )
+      );
+    }
   }
   return arrayWord;
 }
 
 function operationSlangAndStopWord1Data(data, code, map) {
-  
-    let resultWord = "";
-    let result;
-    let tweetSplit = data.split(" ");
-    for (j = 0; j < tweetSplit.length; j++) {
-      let word = map.get(tweetSplit[j]);
-      if (word != undefined) {
-        code == 1 ? (tweetSplit[j] = word) : tweetSplit[j]="";
-      }
+  let resultWord = "";
+  let result;
+  let tweetSplit = data.split(" ");
+  for (j = 0; j < tweetSplit.length; j++) {
+    let word = map.get(tweetSplit[j]);
+    if (word != undefined) {
+      code == 1 ? (tweetSplit[j] = word) : (tweetSplit[j] = "");
     }
-    for (kata of tweetSplit) {
-      resultWord = `${resultWord} ${kata}`;
-    }
+  }
+  for (kata of tweetSplit) {
+    resultWord = `${resultWord} ${kata}`;
+  }
 
-    if (code == 1){
-      resultWord = removeExcesSpace(resultWord.trim());
-     }else{
-      
-        resultWord = removeExcesSpace(resultWord.trim())
-     }
-     return resultWord;
- 
+  if (code == 1) {
+    resultWord = removeExcesSpace(resultWord.trim());
+  } else {
+    resultWord = removeExcesSpace(resultWord.trim());
+  }
+  return resultWord;
 }
 
-function createArrayOfMaps(data,pergi) {
+function createArrayOfMaps(data, pergi) {
   let map = [];
   for (i = 0; i < data.length; i++) {
     map.push(pergi(data[i]));
   }
   return map;
 }
-function Slang(data){
+function Slang(data) {
   return [data.dataValues.tidakbaku, data.dataValues.katabaku];
 }
 
-function Stemming(data){
+function Stemming(data) {
   return [data.dataValues.kata_imbuhan, data.dataValues.kata_dasar];
 }
-function StopWord(data){
+function StopWord(data) {
   return [data.dataValues.kata, 1];
 }
-
-
 
 module.exports = {
   caseFolding,
@@ -228,5 +257,5 @@ module.exports = {
   removeOnlyOneCharacter,
   operationSlangAndStopWord1Data,
   replaceNumberToCharacter,
-  mappingNumberToCharacter
+  mappingNumberToCharacter,
 };
